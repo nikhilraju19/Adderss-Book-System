eb965e9cb9b32852cbff6e1e01f0c8ca7a63910d
Your Name <nikhilvardhanraju@gmail.com>
2025-02-19 10:53:30 +0530

    [Nikhil] Add: python script that implements class concepts to add contact in my my contacts and display them.

--- address_book.py
+++ address_book.py
@@ -1,6 +1,97 @@
 from logging_config import logger
 
 
+class Contact:
+    """
+    This class contains the inbuilt functions to get the contact details of a person
+    """
+    def __init__(self, first_name, last_name, city, state, zip_code, mobile_number, email_id):
+        """
+        Description:
+            This function is a constructor that gets contact details of a person
+        Parameters:
+            self: self refers to the instance of the class
+            first_name: first name of the person
+            last_name: last name of the person
+            city: city of residence
+            state: state of residence
+            zip_code: zip code of area
+            mobile_number: mobile_number of the person
+            email_id: email id of the person
+        Return:
+            None
+        """ 
+        self.first_name = first_name
+        self.last_name = last_name
+        self.city = city
+        self.state = state
+        self.zip_code = zip_code
+        self.mobile_number = mobile_number
+        self.email_id = email_id
+        
+    def __str__(self):
+        """
+        Description:
+            This function controls what should be returned when the class object is represented as a string.
+        Parameters:
+            self: self refers to the instance of the class
+        Return:
+            None
+        """ 
+        return (f"Name: {self.first_name} {self.last_name}\n"
+                f"Address: {self.city}, {self.state} - {self.zip_code}\n"
+                f"Contact Details: Mobile - {self.mobile_number}, Email - {self.email_id}\n")
+        
+class MyContacts:
+    """
+    This class contains the functions for the multiple operations of the my contacts
+    """
+    def __init__(self):
+        """
+        Description:
+            It is a constructor that initializes empty contacts list upon object creation
+        Parameters:
+            self: self refers to the instance of the class
+        Return:
+            None
+        """ 
+        self.contacts = []
+        
+    def add_contact(self, contact):
+        """
+        Description:
+            This function is used to add a contact in the my contacts
+        Parameters:
+            self: self refers to the instance of the class
+            contact: contact that needed to add in my contacts
+        Return:
+            bool: True if contact is added successfully in the my contacts, False otherwise
+        """ 
+        if contact is not None:
+            self.contacts.append(contact)
+            logger.info(f"{contact.first_name} {contact.last_name} added to my contacts")
+            return True
+        logger.warning("Failed to add contact")
+        return False
+    
+    def display_contacts(self):
+        """
+        Description:
+            This function is used to display contacts present in my contacts
+        Parameters:
+            self: self refers to the instance of the class
+        Return:
+            None
+        """ 
+        if not self.contacts:
+            logger.warning("No contacts available to display.")
+            print("No contacts in my contacts")
+        else:
+            print("\nDisplaying contacts present in My Contacts:")
+            for index, contact in enumerate(self.contacts, start = 1):
+                print(f"Contact {index}:")
+                print(f"{contact}")
+        
 def create_contact():
     """
 	Description:
@@ -28,17 +119,18 @@ def create_contact():
                 break
             else:
                 logger.warning("Mobile number should be numeric and it must be exactly 10 digits")
-                print("Mobile number should be numeric and it must be exactly 10 digits")
+                print("Invalid Input: Mobile number should be numeric and it must be exactly 10 digits")
         while True:
             email_id = input("Enter email id: ")
             if '@' in email_id or '.' in email_id:
                 break
             else:
+                logger.warning("Invalid Email id format")
                 print("Invalid Email id format")
                 
     except KeyboardInterrupt:
-        print("\nProcess interrupted by user. Exiting...")
         logger.error("Process interrupted by user.")
+        print("\nProcess interrupted by user. Exiting...")
         exit(1)
     
     except Exception as e:
@@ -48,57 +140,13 @@ def create_contact():
     
     # if all the variables are non empty it creates contact otherwise it returns None
     if all([first_name, last_name, city, state, zip_code, mobile_number, email_id]):
-        contact = {
-            "first_name": first_name,
-            "last_name": last_name,
-            "city": city,
-            "state": state,
-            "zip_code": zip_code,
-            "mobile_number": mobile_number,
-            "email_id": email_id
-        }
+        contact = Contact(first_name, last_name, city, state, zip_code, mobile_number, email_id)
         logger.info(f"{first_name} {last_name} - Contact created successfully ")
         return contact
     else:
         logger.warning("Failed to create contact. Please enter correct details in all the fields")
         return None
-
-def add_contact(my_contacts, contact):
-    """
-	Description:
-		This function is used to add a contact in the my_contacts
-	Parameters:
-        my_contacts: my contacts contains all the contacts at one place
-        contact: contact that needed to add in my contacts
-	Return:
-		bool: True if contact is added successfully in the my contacts, False otherwise
-    """ 
-    if contact:
-        my_contacts.append(contact)
-        logger.info(f"{contact['first_name']} {contact['last_name']} added to my contacts")
-        return True
-    logger.warning("Failed to add contact.")
-    return False
     
-def display_contacts(my_contacts):
-    """
-	Description:
-		This function is used to display contacts present in my contacts
-	Parameters:
-        my_contacts: my_contacts is to display contacts present in them
-	Return:
-		None
-    """ 
-    if not my_contacts:
-        logger.warning("To display contact details please add a contact first")
-        print("No contacts in my contacts")
-    else:
-        print("\nDisplaying contacts in my contacts:")
-        for contact in my_contacts:
-            print(f"Name: {contact['first_name']} {contact['last_name']}")
-            print(f"Address: {contact['city']}, {contact['state']} - {contact['zip_code']}")
-            print(f"Contact Details: Mobile number - {contact['mobile_number']}, Email id - {contact['email_id']}\n")
-
 def main():
     """
 	Description:
@@ -109,11 +157,38 @@ def main():
 	Return:
 		None
     """
-    my_contacts = []
-    contact = create_contact()
-    add_contact(my_contacts, contact)
-    display_contacts(my_contacts)
+    my_contacts = MyContacts()
+    while True:
+        try:
+            print("\nAdress Book Menu: ")
+            print("1. Create a new contact")
+            print("2. Display Contacts")
+            print("3. Exit")
+            
+            choice = int(input("Enter your choice: "))
+            
+            if choice == 1:
+                contact = create_contact()
+                my_contacts.add_contact(contact)
+            elif choice == 2:
+                my_contacts.display_contacts()
+            elif choice == 3:
+                logger.info("Exiting My Contacts System")
+                print("Exiting My Contacts System")
+                break
+            else:
+                logger.warning("Invalid choice. Please enter a valid choice.")
+                print("Invalid choice. Please try again.")
 
+        except KeyboardInterrupt:
+            logger.error("Process interrupted by user.")
+            print("\nProcess interrupted by user. Exiting...")
+            exit(1)
+        
+        except Exception as e:
+            logger.error(f"Unexpected error: {e}")
+            print(f"Unexpected error occured: {e}")
+            return None
 
 if __name__ == "__main__":
     main()
